8bec538478b0b0d08d4752ab45b68ea20a38798d
Atheek Ashkar <atheek99@yahoo.com>
2023-08-06 17:00:34 -0400

    Finished up the Javascript. All of it just works

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const firstName = document.getElementById('firstName')
-const lastName = document.getElementById('lastName')
 const email = document.getElementById('email')
 const phoneNumber = document.getElementById('phoneNumber')
 const password = document.getElementById('password')
@@ -9,11 +7,10 @@ const emailError = document.getElementById('email-error')
 const phoneError = document.getElementById('phone-error')
 const passwordError = document.getElementById('password-error')
 const confirmPassError = document.getElementById('confirmPassword-error')
-
 const createAccount = document.querySelector('.create')
 const fillError = document.getElementById('fill-error')
 
-email.addEventListener("input", function (event) {
+email.addEventListener("input", ()=>{
     if (email.validity.typeMismatch) {
       emailError.textContent = 'Please enter in a valid Email. ex(johnSmith@email.com)';
     } else {
@@ -21,10 +18,29 @@ email.addEventListener("input", function (event) {
     }
   }); 
 
-  createAccount.addEventListener('click', function(){
-    if(firstName.value === ''){
-        fillError.textContent = 'Please fill out all the fields!'
-    }else{
-        fillError.textContent = '';
-    }
-  });
+
+
+
+phoneNumber.addEventListener("input", ()=>{
+  if (phoneNumber.validity.patternMismatch) {
+    phoneError.textContent = 'Please enter in a 10 digit phone number';
+  } else {
+    phoneError.textContent = '';
+  }     
+});
+
+password.addEventListener("input", ()=>{
+  if(password.value === ''){
+    passwordError.textContent = 'Enter a password!'
+  }else{
+    passwordError.textContent = ''
+  }
+});
+
+confirmPassword.addEventListener("input", ()=>{
+  if(confirmPassword.value !== password.value){
+    confirmPassError.textContent = 'This password does not match!'
+  }else{
+    confirmPassError.textContent = '';
+  }
+});
